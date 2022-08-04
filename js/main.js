f3c5252e5ed57006c0937528084ae28574bc4f86
p998sxf <austeja.sirijataviciute@swedbank.lt>
2022-08-04 15:32:48 +0300

    'Init'

---
+++ js/main.js
@@ -0,0 +1,75 @@
+"use strict";
+
+function addMonth(elem) {
+    var annualUseKw = 0, dailyUseKw = 0, i = 0, x = 0;
+    var months = document.getElementById(elem).getElementsByTagName('input');
+
+    for (i = 0; i < months.length; i++) {
+        x = Number(months[i].value);
+        annualUseKw += x;
+    }
+    dailyUseKw = annualUseKw / 365;
+    return dailyUseKw;
+}
+
+function sunHours() {
+    var hrs; //hours of sun
+    var theZone = document.forms.solarForm.zone.selectedIndex;
+    theZone += 1; //because zones starts from 1
+    switch (theZone) {
+        case 1:
+            hrs = 6;
+            break;
+        case 2:
+            hrs = 5.5;
+            break;
+        case 3:
+            hrs = 5;
+            break;
+        case 4:
+            hrs = 4.5;
+            break;
+        case 5:
+            hrs = 4.2;
+            break;
+        case 6:
+            hrs = 3.5;
+            break;
+        default:
+            hrs = 0;
+    }
+    return hrs;
+}
+
+function calculatePanel() {
+    var userChoice = document.forms.solarForm.panel.selectedIndex;
+    var panelOptions = document.forms.solarForm.panel.options;
+    var power = panelOptions[userChoice].value;
+    var name = panelOptions[userChoice].text;
+    var x = [power, name];
+    return x;
+}
+
+function calculateSolar() {
+    var dailyUseKw = addMonth('mpc');
+    var sunHoursPerDay = sunHours();
+    var minKwNeeds = dailyUseKw / sunHoursPerDay;
+
+    var realKwNeeds = minKwNeeds * 1.25;
+    var realWattNeeds = realKwNeeds * 1000;
+    var panelInfo = calculatePanel();
+    var panelOutput = panelInfo[0];
+    var panelName = panelInfo[1];
+    var panelNeeded = Math.ceil(realWattNeeds / panelOutput);
+
+    var feedback = "";
+    feedback += "<p>Based on your average daily use of " + Math.round(dailyUseKw) + " kWh, you will need to purchase " + panelNeeded + " " + panelName + " solar " +
+        "panels to offset 100% of your electricity bill.</p>";
+    feedback += "<h2>Additional details</h2>";
+    feedback += "<p>Your average daily electricity consumption: " + Math.round(dailyUseKw) + " Kwh per day.</p>";
+    feedback += "<p>Average sunshine hours per day: " + sunHoursPerDay + "  hours. </p>";
+    feedback += "<p>Realistic watts need per hour: " + Math.round(realWattNeeds) + " watts/hour.</p>";
+    feedback += "<p>The " + panelName + " panel you selected generates about " + panelOutput + " watts per hour. </p>";
+
+    document.getElementById('feedback').innerHTML = feedback;
+}
